d55221cd439752482a4dc08d5745b6a69002cb24
kirill <cherkiral@gmail.com>
2023-11-16 22:01:16 +0300

    addad non async db url

--- alembic/env.py
+++ alembic/env.py
@@ -8,12 +8,13 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SYNC_DATABASE_URL = os.getenv('SYNC_DATABASE_URL')
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option('sqlalchemy.url', DATABASE_URL)
+config.set_main_option('sqlalchemy.url', SYNC_DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
